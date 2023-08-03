2f5ce152b13374f46c4272bb8cf32f2873c38166
Khanh Tran <ktran@cas.org>
2023-08-03 14:12:43 -0400

    Initial commit

---
+++ test/cdk-memcached.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkMemcached from '../lib/cdk-memcached-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-memcached-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkMemcached.CdkMemcachedStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
