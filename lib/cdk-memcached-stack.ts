6b0d4df8346709ffbde84262452edc17edea5365
Khanh Tran <ktran@cas.org>
2023-08-08 14:52:59 -0400

    Init

--- lib/cdk-memcached-stack.ts
+++ lib/cdk-memcached-stack.ts
@@ -1,16 +1,59 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib'
+import { CfnOutput } from 'aws-cdk-lib'
+import {
+    Peer,
+    Port,
+    SecurityGroup,
+    Subnet,
+    Vpc,
+} from 'aws-cdk-lib/aws-ec2'
+import { CfnCacheCluster, CfnSubnetGroup } from 'aws-cdk-lib/aws-elasticache'
+
+import { Construct } from 'constructs'
+import path = require('path')
 
 export class CdkMemcachedStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props)
 
-    // The code that defines your stack goes here
+        const vpc = Vpc.fromLookup(this, `vpc`, {
+            vpcId: '<YOUR_VPC_ID>',
+        })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkMemcachedQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const subnets = [
+            Subnet.fromSubnetAttributes(this, 'subnet-b', {
+                subnetId: '<YOUR_SUBNET_ID>',
+                availabilityZone: 'us-east-2b',
+            }),
+            Subnet.fromSubnetAttributes(this, 'subnet-c', {
+                subnetId: 'YOUR_ANOTHER_SUBNET_ID',
+                availabilityZone: 'us-east-2c',
+            }),
+        ]
+
+        const cacheSecurityGroup = new SecurityGroup(this, 'cache-security-group', {
+            vpc: vpc,
+        })
+
+        cacheSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.allTcp(), 'Accepts all incomming')
+
+        const cacheSubnetGroup = new CfnSubnetGroup(this, 'cache-subnet-group', {
+            description: 'Subnet group for cache',
+            subnetIds: subnets.map((s) => s.subnetId),
+            cacheSubnetGroupName: 'cache-subnet-group',
+        })
+
+        const memcachedCluster = new CfnCacheCluster(this, 'memcached-cluster', {
+            engine: 'memcached',
+            cacheNodeType: 'cache.t2.small',
+            numCacheNodes: 1,
+            clusterName: 'kxt29-memcached',
+            vpcSecurityGroupIds: [cacheSecurityGroup.securityGroupId],
+            cacheSubnetGroupName: cacheSubnetGroup.cacheSubnetGroupName,
+        })
+        memcachedCluster.addDependency(cacheSubnetGroup)
+
+        new CfnOutput(this, 'endpoint', { value: memcachedCluster.attrConfigurationEndpointAddress })
+        new CfnOutput(this, 'port', { value: memcachedCluster.attrConfigurationEndpointPort })
+    }
 }
